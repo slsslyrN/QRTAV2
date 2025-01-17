d8c819f1f52807b3d2da77fdc9f947a589dba134
carnza <136651251+carnza@users.noreply.github.com>
2025-01-17 18:28:08 -0300

    2do commit

--- src/app/newpass/newpass.page.ts
+++ src/app/newpass/newpass.page.ts
@@ -16,3 +16,4 @@ export class NewpassPage implements OnInit {
   }
 
 }
+
--- src/app/qr-profesor/qr-profesor.page.ts
+++ src/app/qr-profesor/qr-profesor.page.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-qr-profesor',
   standalone: true,
-  imports:[IonicModule],
   templateUrl: './qr-profesor.page.html',
   styleUrls: ['./qr-profesor.page.scss'],
+  imports: [IonicModule],
 })
 export class QrProfesorPage implements OnInit {
+  clase: string = '';
+  id: string = '';
+  horario: string = '';
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private router: Router) {
+    const navigation = this.router.getCurrentNavigation();
+    this.clase = navigation?.extras.state?.['nombre'] || '';
+    this.id = navigation?.extras.state?.['id'] || '';
+    this.horario = navigation?.extras.state?.['horario'] || '';
   }
 
+  ngOnInit() {}
 }
